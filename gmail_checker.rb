c17cb1fd95fe6e6e2a0f77d5952e7924760a9511
Geraint White <geraintwhite@gmail.com>
2014-01-09 22:10:18 +0000

    Update gmail_checker.rb

--- gmail_checker.rb
+++ gmail_checker.rb
@@ -1,5 +1,5 @@
 require 'gmail'
  
-gmail = Gmail.new('geraintwhite@gmail.com', 'shxtlfxfikhcwbje')
+gmail = Gmail.new('example@gmail.com', 'password')
 mail_count = gmail.inbox.count(:unread)
 puts mail_count == 0 ? "No mail" : "Mail: #{mail_count}"
